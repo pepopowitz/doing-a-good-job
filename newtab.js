874ba47c84314352913adfe55b83ec639d5a37c0
Jon Allured <jon.allured@gmail.com>
2023-04-19 21:10:11 -0500

    Set time every second
    Co-authored-by: Steven Hicks <steven.j.hicks@gmail.com>

--- newtab.js
+++ newtab.js
@@ -1,26 +1,9 @@
 'use strict';
 
-const main = document.querySelector('main');
+function setCurrentTime() {
+  const timePara = document.querySelector('main p');
+  timePara.innerText = new Date().toLocaleTimeString();
+};
 
-// 2raddads -- this is how we inject dynamic info into the screen
-// const createTop = () => {
-//   const div = document.createElement('div');
-//   div.className = 'colorFun';
-//   div.innerText = '2raddads';
-//   main.appendChild(div);
-// };
-
-// createTop();
-
-// 2raddads - this is maybe how we'll do local storage
-// function setToDo() {
-//   chrome.storage.sync.get(['todo']).then((value) => {
-//     if (!value.todo) {
-//       todo.innerText = '';
-//     } else {
-//       todo.innerText = value.todo;
-//     }
-//   });
-// }
-
-// setToDo();
+setCurrentTime();
+setInterval(setCurrentTime, 1000);
