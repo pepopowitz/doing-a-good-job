a6782d4cca3ff3f181cc148b4665ea637d0420e2
Steven Hicks <steven.j.hicks@gmail.com>
2023-06-15 13:11:48 -0500

    some logging to show that the form was actually hooked up correctly

--- popup/praiseForm.js
+++ popup/praiseForm.js
@@ -1,3 +1,5 @@
+console.log('hiiii');
+
 const handleConfigSubmit = (event) => {
   event.preventDefault();
   // TODO: finish connecting this form submit handler!!!
@@ -9,17 +11,23 @@ const handleConfigSubmit = (event) => {
   //   accumulator[inputTag.name] = inputTag.value;
   //   return accumulator;
   // }, {});
-
-  chrome.storage.local.set({ praiseMarkdown: 'steve and jon' });
+  console.log(chrome.storage.local);
+  chrome.storage.local.set({ steve: 'cool' });
+  console.log('it should be set?');
 };
 
 const initializeForm = () => {
+  console.log('yooooooo');
   const [form] = document.getElementsByTagName('form');
   // form.addEventListener("reset", handleConfigReset);
   form.addEventListener('submit', handleConfigSubmit);
   // setFormValues();
 
   // setListItems();
+
+  chrome.storage.local.get(['steve']).then((result) => {
+    console.log('Value currently is ' + JSON.stringify(result, null, 2));
+  });
 };
 
 window.addEventListener('load', initializeForm);
