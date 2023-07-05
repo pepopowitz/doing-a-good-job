d5c066ffb368efd396d977a4710f20a809df2b28
Steven Hicks <steven.j.hicks@gmail.com>
2023-07-05 09:31:20 -0500

    hook up praiseMarkdown field
    Co-authored-by: Pavlos Vinieratos <pvinis@gmail.com>

--- popup/praiseForm.js
+++ popup/praiseForm.js
@@ -3,31 +3,27 @@ console.log('hiiii');
 const handleConfigSubmit = (event) => {
   event.preventDefault();
   // TODO: finish connecting this form submit handler!!!
-  console.log('yoooooo', event);
+  const praiseElement = document.querySelector('#praiseMarkdown');
+  const praiseValue = praiseElement.value;
 
-  // const inputTags = Array.from(document.querySelectorAll("input[type='text']"));
-
-  // const config = inputTags.reduce((accumulator, inputTag) => {
-  //   accumulator[inputTag.name] = inputTag.value;
-  //   return accumulator;
-  // }, {});
-  console.log(chrome.storage.local);
-  chrome.storage.local.set({ steve: 'cool' });
-  console.log('it should be set?');
+  chrome.storage.local.set({ praiseMarkdown: praiseValue });
+  readValue();
 };
 
+function readValue() {
+  chrome.storage.local.get('praiseMarkdown').then((result) => {
+    console.log('Value currently is ' + JSON.stringify(result, null, 2));
+  });
+}
+
 const initializeForm = () => {
-  console.log('yooooooo');
   const [form] = document.getElementsByTagName('form');
   // form.addEventListener("reset", handleConfigReset);
   form.addEventListener('submit', handleConfigSubmit);
   // setFormValues();
 
   // setListItems();
-
-  chrome.storage.local.get(['steve']).then((result) => {
-    console.log('Value currently is ' + JSON.stringify(result, null, 2));
-  });
+  readValue();
 };
 
 window.addEventListener('load', initializeForm);
